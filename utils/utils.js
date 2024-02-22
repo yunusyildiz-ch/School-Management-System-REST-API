2a71c1902dbd1c64948d6a6546f23c2ce67f6fff
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-02-22 01:08:31 +0100

    "build: install crypto and create calculateFileHash"

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,30 @@
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import fs from "fs";
 
-
- const generateToken = (user) => {
+const generateToken = (user) => {
   return jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
 };
 
+function calculateFileHash(filePath) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash("sha256");
+    const stream = fs.createReadStream(filePath);
 
+    stream.on("data", (data) => {
+      hash.update(data, "utf8");
+    });
 
+    stream.on("end", () => {
+      resolve(hash.digest("hex"));
+    });
 
-export default generateToken;
+    stream.on("error", (err) => {
+      reject(err);
+    });
+  });
+}
+
+export { generateToken, calculateFileHash };
