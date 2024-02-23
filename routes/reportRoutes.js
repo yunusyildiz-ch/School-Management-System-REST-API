72e7fdbdf5aa1a92a5a2cbf28d6409e004f0f769
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-02-23 17:36:54 +0100

    "feat: passport authentication and isAdminOrAssistantOrTeacher middleware for attendance report"

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
+import passport from "../config/passport.js";
+import {
+  isAdminOrAssistant,
+  isAdminOrAssistantOrTeacher,
+  isTeacher,
+} from "../middlewares/authMiddleware.js";
 import reportController from "../controllers/reportController.js";
 
 const router = express.Router();
 
 router.get(
   "/attendance-report/:classId",
+  passport.authenticate("jwt", { session: false }),
+  isAdminOrAssistantOrTeacher,
   reportController.downloadAttendanceReport
 );
 
