0ef552cfec5be1d4530e307a888c02cfaf86c015
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2024-03-03 15:00:56 +0100

    "style: welcome.html"

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import Express from "express";
 import cors from "cors";
 import Morgan from "morgan";
 import "dotenv/config";
-import  {connectDB} from "./config/db.js";
+import { connectDB } from "./config/db.js";
 //todo : ? import './config/multerConfig.js'
-import  './models/index.js'
+import "./models/index.js";
 import { createAdminUser } from "./config/setup.js";
 import path from "path";
 import fs from "fs";
@@ -12,8 +12,11 @@ import yaml from "yaml-js";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import passport from "./config/passport.js";
-import  {setupAssignmentsCronJobs,setupScheduleCronJobs}  from "./jobs/scheduler.js";
-import { fileURLToPath } from 'url';
+import {
+  setupAssignmentsCronJobs,
+  setupScheduleCronJobs,
+} from "./jobs/scheduler.js";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import classRoutes from "./routes/classRoutes.js";
@@ -28,7 +31,7 @@ import reportRoutes from "./routes/reportRoutes.js";
 const app = Express();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const openApiYamlPath = path.join(__dirname, "./SchoolManSystemOpenAPI.yaml")
+const openApiYamlPath = path.join(__dirname, "./SchoolManSystemOpenAPI.yaml");
 const openapiObject = yaml.load(fs.readFileSync(openApiYamlPath, "utf8"));
 
 app.use(cors());
@@ -37,12 +40,14 @@ app.use(Express.urlencoded({ extended: true }));
 app.use(Morgan("dev"));
 app.use(passport.initialize());
 
-app.use(Express.static(path.join(__dirname,'public')));
+app.use(Express.static(path.join(__dirname, "public")));
 
 setupAssignmentsCronJobs();
 setupScheduleCronJobs();
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, './public', 'welcome.html'))});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "welcome.html"));
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/class", classRoutes);
@@ -54,31 +59,28 @@ app.use("/api/class-schedule", classScheduleRoutes);
 app.use("/api/report", reportRoutes);
 
 //todo: app.use("/api/attendance", attendanceRoutes);
-app.use("/api/file",fileRoutes)
+app.use("/api/file", fileRoutes);
+
+// const CSS_URL =   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const options = {
-  swaggerDefinition:openapiObject,
-  apis:['./routes/*.js']
-}
+  swaggerDefinition: openapiObject,
+  apis: ["./routes/*.js"],
+  // customCssUrl: CSS_URL
+};
 
-
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-const spacs = swaggerJsDoc(options)
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spacs,{ customCssUrl: CSS_URL }));
-
-
+const swaggerSpec = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 connectDB()
   .then(async () => {
-    await createAdminUser().then(async () =>{
-      app.listen(process.env.EXPRESS_PORT)
-    })
-    
+    await createAdminUser().then(async () => {
+      app.listen(process.env.EXPRESS_PORT);
+    });
   })
   .catch((error) => {
     console.error("Database connection error: " + error.message);
     process.exit(1);
   });
-
 
 export default app;
